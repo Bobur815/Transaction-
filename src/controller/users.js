09ebe0c2b6a2e78b40751cbb345a7551bc091023
Yandiyev <azizbekyandiev@gmail.com>
2025-04-23 13:44:19 +0500

    GET & POST

--- src/controller/users.js
+++ src/controller/users.js
@@ -18,6 +18,30 @@ const GET_USERS = (req,res) => {
 }
 
 const POST = (req,res) => {
+    try {
+        let users = JSON.parse(fs.readFileSync(path.resolve("src","db/users.json")));
+        let {balance,monthlyLimit} = req.body;
+        if(!balance || balance < 1 || isNaN(+balance)){
+            throw new Error("Invalid Balance!");
+        }
+        else if(!monthlyLimit || monthlyLimit < 1 || isNaN(+monthlyLimit)){
+            throw new Error("Invalid monthlyLimit!");
+        }
+        let id = users.length ? parseInt(users.at(-1).userId) + 1 : 1;
+
+        let obj = {
+            'userId': String(id),
+            balance,
+            monthlyLimit,
+            "usedLimit" : 0 
+        }
+        users.push(obj);
+        fs.writeFileSync(path.resolve("src","db/users.json"),JSON.stringify(users,null,4))
+        res.status(201).send("Seccessfully!");
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+
     
 }
 const PUT = (req,res) => {
